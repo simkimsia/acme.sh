4863ad5df9cf48a21e3b62b741427a9918c905ee
KimSia Sim <kimcity@gmail.com>
2018-08-11 17:14:46 +0800

    update the code to ensure that namecheap api dns calls can work
    added private method to break up the host record
    _acme-challenge.www.domain.com into 3 parts
    SLD which is domain
    TLD which is com
    Hostname which is _acme-challenge

--- dnsapi/dns_namecheap.sh
+++ dnsapi/dns_namecheap.sh
@@ -5,11 +5,21 @@
 # Created: 2018-07-27
 # version: 0.1
 
+# Please go to Live Chat with NameCheap 
 Namecheap_Actual_Api="https://api.namecheap.com/xml.response"
+# You need to register a separate sandbox account at https://www.sandbox.namecheap.com/myaccount/signup.aspx
 Namecheap_Sandbox_Api="https://api.sandbox.namecheap.com/xml.response"
 
 ## Change to sandbox if you want to test
 Namecheap_Api = Namecheap_Sandbox_Api
+
+## The API key can be found at namecheap.com > Profile section, select Tools and choose the Namecheap API Access option for Business & Dev Tools.
+## API User is the account username associated with the API key
+## UserName is the username of the account you are making changes on
+
+## another thing to note is that you need to whitelist the IP address where you are calling the command from
+
+## the code is basically `xml.response?ApiUser=<api_username>&ApiKey=<api_key>&UserName=<nc_username>&Command=<cmd_name>`
 
 ########  Public functions #####################
 
@@ -20,6 +30,8 @@ dns_namecheap_add() {
 
   Namecheap_Key="${Namecheap_Key:-$(_readaccountconf_mutable Namecheap_Key)}"
 
+  # run `export Namecheap_Key="API_KEY_HERE"` in terminal to ensure this works
+  # to find your API_KEY_HERE go to https://ap.www.namecheap.com/settings/tools/apiaccess/
   if [ -z "$Namecheap_Key" ]; then
     Namecheap_Key=""
     _err "You don't specify namecheap api key yet."
@@ -106,4 +118,40 @@ _Namecheap_request() {
   fi
   _debug2 response "$response"
   _contains "$response" "<is_ok>OK:"
+}
+
+# inspired by dns_me.sh private method _get_root()
+#_acme-challenge.www.domain.com
+#returns
+# _sub_domain=_acme-challenge.www
+# _domain=domain.com
+# _domain_id=sdjkglgdfewsdfg
+_get_root() {
+  domain=$1
+  i=2
+  p=1
+  while true; do
+    h=$(printf "%s" "$domain" | cut -d . -f $i-100)
+    if [ -z "$h" ]; then
+      #not valid
+      return 1
+    fi
+
+    if ! _me_rest GET "name?domainname=$h"; then
+      return 1
+    fi
+
+    if _contains "$response" "\"name\":\"$h\""; then
+      _domain_id=$(printf "%s\n" "$response" | _egrep_o "\"id\":[^,]*" | head -n 1 | cut -d : -f 2 | tr -d '}')
+      if [ "$_domain_id" ]; then
+        _sub_domain=$(printf "%s" "$domain" | cut -d . -f 1-$p)
+        _domain="$h"
+        return 0
+      fi
+      return 1
+    fi
+    p=$i
+    i=$(_math "$i" + 1)
+  done
+  return 1
 }
